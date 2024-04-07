795b8dd1b9665d71e930a904909edeb28daee5b8
Anh <97702902+hihaianh@users.noreply.github.com>
2024-04-07 18:57:45 +0100

    changed list structure
    to install dynamic hover effects

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const defaultContainer = document.getElementById('default-container');
-const contactMeContainer = document.getElementById('nav-item-four-container');
-const contactMe = document.getElementById('item_four');
+const navItem = document.querySelectorAll(".nav-item")
+const navItemAnchor = document.querySelectorAll(".nav-item a")
+const navItemLine = document.querySelectorAll(".nav-line")
+const navItemText = document.querySelectorAll("span")
 
-contactMe.addEventListener('click', function () {
-    if (contactMeContainer.style.display === 'none') {
-        contactMeContainer.style.display === 'block';
-        defaultContainer.style.display === 'none';
-    } else {
-        contactMeContainer.style.display === 'none';
-        defaultContainer.style.display === 'block'
-    }
+navItem.forEach(function(navItem) {
+    navItem.addEventListener("click", function() {
+        navItem.style.transform = "translateX(30px)";
+        // keep track of last clicked nav item and listen for next nav item 
+        // unclick last nav item (+ apply original styling) and 
+        // apply clicked changes to new nav item
+    })
 })
